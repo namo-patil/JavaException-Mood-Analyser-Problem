d05c3895a8906abece898b1347bb41ced9b97f1f
namo-patil <namratapatil74738@gmail.com>
2022-05-18 15:44:34 +0530

    Refactored: Test case to analyse Happy or Sad Mood

--- src/main/java/com/bridgelabz/customexceptions/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/customexceptions/MoodAnalyser.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.customexceptions;
 
 public class MoodAnalyser {
-    public String analyseMood(String message) {
+    private String message;
+
+    public MoodAnalyser() {
+
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
         if (message.contains("Sad")) {
             return "SAD";
         } else {
--- src/test/java/com/bridgelabz/customexceptions/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/customexceptions/MoodAnalyserTest.java
@@ -7,15 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
         assertEquals("SAD", mood);
     }
 
     @Test
     public void givenMessage_WhenHappy_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        String mood = moodAnalyser.analyseMood();
         assertEquals("HAPPY", mood);
     }
 }
