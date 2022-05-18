e466b77075f4e27677a1939f0ec03c557b68b221
namo-patil <namratapatil74738@gmail.com>
2022-05-18 15:21:29 +0530

    Added: Test case to analyse Happy or Sad Mood

---
+++ src/test/java/com/bridgelabz/customexceptions/MoodAnalyserTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.customexceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoodAnalyserTest {
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
+        assertEquals("HAPPY", mood);
+    }
+}
